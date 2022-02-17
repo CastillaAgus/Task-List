a51a1d364046625487e12905b506fab90dc3a5d8
CastillaAgus <riopatagonico@gmail.com>
2022-02-16 21:52:00 -0300

    Agregamos mosk de tareas y la interfaz de las mismas

---
+++ src/app/mosk-tasks.ts
@@ -0,0 +1,20 @@
+import {Task} from "./Task"
+
+
+export const TASK: Task [] = [ 
+    { id: 1,
+    text: "Terminar nuestro primer módulo de Angular.",
+    day:  "Agosto 5 a las 12:00",
+    reminder:true,
+    },
+    { id: 2,
+        text: "Hacer Tarea Clase Sincronica.",
+        day:  "Agosto 5 a las 12:00",
+        reminder:true,
+        },
+    { id: 3,
+        text: "Libro Javascript",
+        day:  "Agosto 5 a las 19:00",
+        reminder:false,
+            }
+]
